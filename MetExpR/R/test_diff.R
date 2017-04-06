a1e38137b157b3fd711600cb2ae8da1fcadefe7c
Aleksandra Dabrowska <oll.dabrowska@gmail.com>
2017-04-06 10:15:48 +0200

    test_diff function

--- MetExpR/R/test_diff.R
+++ MetExpR/R/test_diff.R
@@ -1,25 +1,28 @@
 #' @title test_diff
 #'
-#' @description Function \code{tet_diff} computes statistics and p-values for choosen test.
+#' @description Function \code{test_diff} computes statistics and p-values for choosen test.
 #'
 #' @param data data frame of ... Columns corresponds to genes, rows to samples.
 #' @param condition vector of levels coresponding to order of samples in data.
 #' @param test values: ttest, nbinom.
 #'
 #' @return A data frame with the following columns:
-#'  \item{id}{The ID of the observable, taken from the row names of the counts slots.}
-#'  \item{exp.mean}{The base mean.}
-#'  \item{exp.mean.gr1}{The base mean for the counts for the first condition.}
-#'  \item{exp.mean.g2}{The base mean for the second condition.}
-#'  \item{exp.fold}{The ratio mean1/mean2.}
-#'  \item{exp.log.fold}{The log2 of the fold change.}
-#'  \item{exp.pval}{The p value for rejecting the null hypothesis 'mean1==mean2'}
-#'  \item{exp.padj}{The adjusted p values (adjusted with 'p.adjust( pval, method="BH")')}
+#'  \item{id}{The ID of the observable, taken from the row names of the counts slots}
+#'  \item{mean}{The base mean}
+#'  \item{log.fold}{The log2 of the fold change}
+#'  \item{pval}{The p values for rejecting the null hypothesis such that means in both groups are equal}
+#'  \item{padj}{The adjusted p values}
 #'
 #' @export
 
 test_diff <- function(data, condition, test="ttest"){
-  if(test=="ttest") res = mety_ttest(data, condition)
-  if(test=="nbinom") res = expr_nbinom(data, condition)
+  if(test=="ttest"){
+     res <- mety_ttest(data, condition)
+     res <- res[,c(7,2,1,4,5)]
+  }
+  if(test=="nbinom"){ 
+    res <- expr_nbinom(data, condition)
+    res <- res[,c(1,2,6,7,8)]
+  }
   return(res)
 }
