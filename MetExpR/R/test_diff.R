e9b3510e08ce51ea5a41d6fde8fc8168cc1b5549
Alicja Gosiewska <alicjagosiewska@gmail.com>
2017-04-22 16:12:48 +0200

    test_diff doc

--- MetExpR/R/test_diff.R
+++ MetExpR/R/test_diff.R
@@ -1,10 +1,12 @@
-#' @title test_diff: statistical computations for data from methylation or expression
+#' @title test_diff: statistical computations for data from methylation or expression.
 #'
-#' @description Function \code{test_diff} computes statistics and p-values for choosen test. The function can use two test: t-test for methylation data and negative binomial test for data from gene expression. By default function \code{test_diff} calls the \code{mety_ttest} for methylation. 
+#' @description Function \code{test_diff} computes statistics and p-values for choosen test.
+#' The function provides use two tests: t-test and negative binomial test for data from gene expression.
+#' By default function \code{test_diff} calls the \code{mety_ttest}.
 #'
 #' @param data data frame containing values of methylation or the raw counts of sequencing reads. Columns corresponds to genes, rows to samples.
 #' @param condition vector of levels coresponding to order of samples in data.
-#' @param test values: ttest, nbinom.
+#' @param test variable defining test. Values: ttest, nbinom.
 #'
 #' @return A data frame with the following columns:
 #'  \item{id}{The ID of the observable, taken from the row names of the counts slots.}
@@ -19,13 +21,13 @@
 test_diff <- function(data, condition, test="ttest"){
   if(test=="ttest"){
      res <- mety_ttest(data, condition)
-     res <- res[,c(1,2,3,4,5)]
-     colnames(res) <- c("id","mean","log.fold","pval","padj")
- }
-  if(test=="nbinom"){ 
-    res <- expr_nbinom(data, condition)
-    res <- res[,c(1,2,3,4,5)]
-    colnames(res) <- c("id","mean","log.fold","pval","padj")
   }
+  if(test=="nbinom"){
+    res <- expr_nbinom(data, condition)
+  }
+
+  res <- res[,c(1,2,3,4,5)]
+  colnames(res) <- c("id","mean","log.fold","pval","padj")
+
   return(res)
 }
