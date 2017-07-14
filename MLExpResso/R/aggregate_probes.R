69535ff8d59de69bc4453d3649e3d03c99f027d1
Aleksandra Dąbrowska <oll.dabrowska@gmail.com>
2017-07-14 12:12:29 +0200

    keep argument for selected column - fixed

--- MLExpResso/R/aggregate_probes.R
+++ MLExpResso/R/aggregate_probes.R
@@ -3,7 +3,8 @@
 #' @description Function \code{aggregate_probes} aggregates CpG probes to corresponding genes.
 #'
 #' @param data data frame  containing methylation values for CpG probes. Columns corresponds to probes, rows to samples.
-#'
+#' @param keep the name of the column we want to keep
+#' 
 #' @return A data frame with CpG probes mapped to genes. If there were more than one probe corresponding to a gene, value is a mean of those probes.
 #'
 #'@examples
@@ -12,16 +13,21 @@
 #'}
 #' @export
  
-aggregate_probes <- function(data, keep=""){
+aggregate_probes <- function(data, keep=NULL){
+  if(!is.null(keep)){
+    keep_column <- as.data.frame(data[,keep])
+    colnames(keep_column) <- keep
+  }
   genom <- illumina_humanmethylation_27_data[,c(1,11)]
   dict_cg <- as.vector(genom$Symbol)
   names(dict_cg) <- genom$Name
   rnames<-rownames(data)
-  keep_column <- as.data.frame(data[,keep])
-  colnames(keep_column) <- keep
+  
   colnames(data) <- dict_cg[colnames(data)]
   data<-as.data.frame(lapply(split(as.list(data),f = colnames(data)),function(x) Reduce(`+`,x) / length(x)))
   rownames(data) <- rnames
+  if(!is.null(keep)){
   data <- cbind(keep_column, data)
+  }
   return(data)
 } 
